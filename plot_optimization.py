95ef6f415d4ea1d6863008d4723b9803100cb496
Mauro Verzetti <mverzett@cern.ch>
2015-06-10 10:15:55 -0500

    added fit and best point choice

--- plot_optimization.py
+++ plot_optimization.py
@@ -8,11 +8,15 @@ from URAnalysis.Utilities.roottools import ArgSet, ArgList
 from pdb import set_trace
 import logging
 import os
+from URAnalysis.PlotTools.Plotter import Plotter
+import math
 
 asrootpy = rootpy.asrootpy
 rootpy.log["/"].setLevel(rootpy.log.INFO)
 ROOT.gStyle.SetOptTitle(0)
 ROOT.gStyle.SetOptStat(0)
+ROOT.gROOT.SetBatch(True)
+ROOT.gStyle.SetOptFit(11111)
 
 import URAnalysis.Utilities.prettyjson as prettyjson
 from argparse import ArgumentParser
@@ -31,10 +35,14 @@ results.sort(key=lambda x: x['median'])
 
 nevts_graph = plotting.Graph(len(results))
 upbound_graph = plotting.Graph(len(results))
+max_unc = 0.
+bound_range = results[-1]['up_edge'] - results[0]['up_edge']
 
 for idx, info in enumerate(results):
    nevts_graph.SetPoint(idx, info['median'], info["one_sigma"]["relative"])
    upbound_graph.SetPoint(idx, info['up_edge'], info["one_sigma"]["relative"])
+   if info["one_sigma"]["relative"] > max_unc:
+      max_unc = info["one_sigma"]["relative"]
 
 canvas = plotting.Canvas()
 nevts_graph.Draw('APL')
@@ -47,6 +55,42 @@ canvas.SaveAs(
 upbound_graph.Draw('APL')
 upbound_graph.GetXaxis().SetTitle('upper bin edge')
 upbound_graph.GetYaxis().SetTitle('relative fit uncertainty')
+
+tf1 = Plotter.parse_formula('scale / (x - shift)', 'scale[1,0,10000],shift[0,0,1000]')
+# ROOT.TF1('ret', '[0]/(x - [1])', -3, 1000)
+tf1.SetRange(0, 1000)
+tf1.SetLineColor(ROOT.EColor.kAzure)
+tf1.SetLineWidth(3)
+result = upbound_graph.Fit(tf1, 'MES') #WL
+
+scale = tf1.GetParameter('scale') 
+shift = tf1.GetParameter('shift') 
+
+distance = float('inf')
+min_idx = -1
+distances = []
+for idx, info in enumerate(results):
+   new_dist = math.sqrt(
+      ((info['up_edge'] - shift)/bound_range)**2 + 
+      (info["one_sigma"]["relative"]/max_unc)**2
+      )
+   distances.append(new_dist)
+   #print idx, new_dist, distance
+   if new_dist < distance:
+      distance = new_dist
+      min_idx  = idx
+
+#max_unc = float('inf')
+#bound_range = results[-1]['up_edge'] - results[0]['up_edge']
+#print results[min_idx]['up_edge'], results[min_idx]["one_sigma"]["relative"]
+
+upbound_best = plotting.Graph(1)
+upbound_best.SetPoint(0, results[min_idx]['up_edge'], results[min_idx]["one_sigma"]["relative"])
+upbound_best.markerstyle = 29
+upbound_best.markersize  = 3
+upbound_best.markercolor = 2
+upbound_best.Draw('P same')
+
 canvas.SaveAs(
    os.path.join(args.outdir, 'upbound_%s.png' % args.var)
    )
