033906550e1a7682ee504a7c856a757f35c16acf
Mauro Verzetti <mverzett@cern.ch>
2015-07-29 06:44:23 -0500

    addded array to run different regularization modes and unified the command

--- run_unfold_distribution.sh
+++ run_unfold_distribution.sh
@@ -5,9 +5,9 @@
 echo jobid is "$jobid"
 
 # Suffix is an editable string used to put the results of different jobs in different directories
-suffix="_150630-2"
+suffix="_27Jul2015"
 # This array stores the variables for which the unfilding needs to be done
-variable_array=('ptthad_2015Jun26_skewed' 'ptthad')
+variable_array=('thadpt')
 # Array of switches to turn on or off the use of the covariant matrix...
 cov_matrix_array=('--cov_matrix=full')
 # ...and corresponding strings to name the directories where the results will go
@@ -23,7 +23,8 @@ reco_truth_array=(' ')
 # Array of transport (or "migration") matrices to use (this is also the directory name)...
 scaled_matrix_array=('notscaled')
 # ...and array of files in which each matrix is stored
-scaled_matrix_file_array=('plots/${jobid}/ttxsec/migration_matrices.root')
+scaled_matrix_file_array=("plots/${jobid}/ttxsec/migration_matrices.root")
+regmodes=('Curvature')
 # # Array of biases to apply to the true distribution
 # bias_id_array=('_2015Jun26_skewed' '')
 # #...and corresponding strings to name the directories where the results will go
@@ -49,10 +50,15 @@ do
           else
             fit_file_name=${fit_file_name_array[$fit_file_index]}
           fi
-          echo mkdir plots/${jobid}/ttxsec/${variable}_${cov_matrix_name_array[${cov_matrix_index}]}_reco${fit_file_name}_${scaled_matrix_array[$scaled_matrix_index]}${suffix}
-          mkdir plots/${jobid}/ttxsec/${variable}_${cov_matrix_name_array[${cov_matrix_index}]}_reco${fit_file_name}_${scaled_matrix_array[$scaled_matrix_index]}${suffix}
-          echo python unfold_distribution.py ${variable} plots/${jobid}/ttxsec/${variable}/${variable}.${fit_file_array[$fit_file_index]} ${scaled_matrix_file_array[$scaled_matrix_index]} ${cov_matrix_array[$cov_matrix_index]} ${reco_truth_array[$reco_truth_index]} -d plots/${jobid}/ttxsec/${variable}_${cov_matrix_name_array[${cov_matrix_index}]}_reco${fit_file_name}_${scaled_matrix_array[$scaled_matrix_index]}${suffix}
-          python unfold_distribution.py ${variable} plots/${jobid}/ttxsec/${variable}/${variable}.${fit_file_array[$fit_file_index]} ${scaled_matrix_file_array[$scaled_matrix_index]} ${cov_matrix_array[$cov_matrix_index]} ${reco_truth_array[$reco_truth_index]} -d plots/${jobid}/ttxsec/${variable}_${cov_matrix_name_array[${cov_matrix_index}]}_reco${fit_file_name}_${scaled_matrix_array[$scaled_matrix_index]}${suffix}
+					for regmode in "${regmodes[@]}"
+					do
+							target_dir=plots/${jobid}/ttxsec/${variable}_${cov_matrix_name_array[${cov_matrix_index}]}_reco${fit_file_name}_${scaled_matrix_array[$scaled_matrix_index]}_${regmode}${suffix}
+							echo mkdir $target_dir
+							mkdir plots/${jobid}/ttxsec/${variable}_${cov_matrix_name_array[${cov_matrix_index}]}_reco${fit_file_name}_${scaled_matrix_array[$scaled_matrix_index]}_${regmode}${suffix}
+							cmd="unfold_distribution.py ${variable} plots/$jobid/ttxsec/${variable}/${variable}.${fit_file_array[$fit_file_index]} ${scaled_matrix_file_array[$scaled_matrix_index]} ${cov_matrix_array[$cov_matrix_index]} ${reco_truth_array[$reco_truth_index]} -d ${target_dir} --reg_mode=$regmode --runHandmade"
+							echo python $cmd
+							python $cmd
+					done
         done
       done
     done
