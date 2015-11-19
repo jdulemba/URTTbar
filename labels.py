232fc5d0e322f16e87455c4a9ad79275750a6718
mauro verzetti <mverzett@cern.ch>
2015-11-19 08:05:31 -0600

    update labels

--- labels.py
+++ labels.py
@@ -4,6 +4,11 @@ labels = {
    'tleppt'  : 'p_{T}(t_{lep}) [GeV]',
    'thadeta' : '|#eta(t_{had})|',
    'tlepeta' : '|#eta(t_{lep})|',
+   'tlepy'   : '|y(t_{lep})|',
+   'thady'   : '|y(t_{had})|',
+   'tty'     : '|y(tt)|',
+   'ttpt'    : 'p_{T}(tt) [GeV]',
+   'ttM'     : 'M(tt) [GeV]',
 }
 def set_pretty_label(variable):
    return labels.get(variable, variable)
