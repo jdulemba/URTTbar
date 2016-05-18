21f4345dbd22afab13f2ee87fc10b05016f0eca5
mauro verzetti <mverzett@cern.ch>
2016-05-18 09:44:11 -0500

    bugfix

--- environment.sh
+++ environment.sh
@@ -15,17 +15,7 @@ export URA_NTHREADS=1
 #HERE ARE LIONS!
 #This part should be handled automatically by the scripts,
 #touch it carefully
-export URA_PROJECT=$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd -P)
+export URA_PROJECT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 #source URA env
-ura_dir=`dirname $URA_PROJECT`
+ura_dir=`dirname $(dirname $URA_PROJECT)`
 source $ura_dir/URAnalysis/environment.sh
-export PATH=$URA_PROJECT/bin:$PATH
-
-#FNAL PATCH
-if [[ $HOSTNAME == *"fnal.gov"* ]]
-then
-  export LD_LIBRARY_PATH=/cvmfs/cms.cern.ch/$SCRAM_ARCH/external/boost/1.57.0/lib/:$LD_LIBRARY_PATH
-  export LD_LIBRARY_PATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc481/external/lhapdf6/6.1.5/lib:$LD_LIBRARY_PATH
-fi
-
-export LHAPDF_DATA_PATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc481/external/lhapdf6/6.1.5/share/LHAPDF
