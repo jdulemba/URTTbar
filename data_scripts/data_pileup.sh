9886aff143cac891e996e4a90fa9167d1ebe9e98
jdulemba <josdrw9@gmail.com>
2019-03-05 07:12:30 -0600

    Changed input/output paths to absolute instead of relative

--- data_scripts/data_pileup.sh
+++ data_scripts/data_pileup.sh
@@ -1,27 +1,27 @@
-dir=../inputs/$jobid/INPUT
+outdir=$URA_PROJECT/inputs/$jobid/INPUT
 
 echo "---- Creating PILEUP files combining ----"
 
-for i in $(ls ../inputs/$jobid/data_*.run.json); do
+for i in $(ls $URA_PROJECT/inputs/$jobid/data_*.run.json); do
         filename=$(basename "$i")
         name="${filename%.*}"
         name="${name%.*}"
         echo "   $name"
 done
-echo -e "\n  from ../inputs/$jobid\n"
+echo -e "\n  from $URA_PROJECT/inputs/$jobid\n"
 
-pileup_latest=pileup_latest_2018.txt
+pileup_latest=$URA_PROJECT/data_scripts/pileup_latest_2018.txt
 echo -e "Pileup file used: $pileup_latest\n"
 
-mergeJSON.py ../inputs/$jobid/data_*.run.json  --output=data_all.json
+mergeJSON.py $URA_PROJECT/inputs/$jobid/data_*.run.json  --output=data_all.json
 
-./working_puCalc.py -i data_all.json --inputLumiJSON $pileup_latest --calcMode true --minBiasXsec 69000 --maxPileupBin 100 --numPileupBins 100 $dir/data.meta.pu.root
+./working_puCalc.py -i data_all.json --inputLumiJSON $pileup_latest --calcMode true --minBiasXsec 69000 --maxPileupBin 100 --numPileupBins 100 $outdir/data.meta.pu.root
 echo -e "\n    data.meta.pu.root\n"
 
-./working_puCalc.py -i data_all.json --inputLumiJSON $pileup_latest --calcMode true --minBiasXsec 72450 --maxPileupBin 100 --numPileupBins 100 $dir/data.meta.pu_up.root
+./working_puCalc.py -i data_all.json --inputLumiJSON $pileup_latest --calcMode true --minBiasXsec 72450 --maxPileupBin 100 --numPileupBins 100 $outdir/data.meta.pu_up.root
 echo -e "\n    data.meta.pu_up.root\n"
 
-./working_puCalc.py -i data_all.json --inputLumiJSON $pileup_latest --calcMode true --minBiasXsec 65550 --maxPileupBin 100 --numPileupBins 100 $dir/data.meta.pu_down.root
-echo -e "\n    data.meta.pu_down.root\n have been created and placed in $dir"
+./working_puCalc.py -i data_all.json --inputLumiJSON $pileup_latest --calcMode true --minBiasXsec 65550 --maxPileupBin 100 --numPileupBins 100 $outdir/data.meta.pu_down.root
+echo -e "\n    data.meta.pu_down.root\n have been created and placed in $outdir"
 
 rm data_all.json
