e431904c41f44ee3e0d05c64d757718314a47d4f
jdulemba <josdrw9@gmail.com>
2019-03-11 09:22:23 -0500

    Added flexibility for making electron and muon SF files

--- inputs/data/SFMerging/merge_sf.py
+++ inputs/data/SFMerging/merge_sf.py
@@ -5,10 +5,19 @@ that does not permeates in this world, everything is hardcoded.
 
 Enjoy
 '''
-from rootpy.io import root_open, File
+from rootpy.io import root_open
 from rootpy.plotting import Hist, Hist2D
 from pdb import set_trace
-from ROOT import TObjString, TFile
+from ROOT import TObjString
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('lepton', help='Choose between electron or muon.')
+args = parser.parse_args()
+
+lchoices = ['muon', 'electron']
+if args.lepton not in lchoices:
+    raise RuntimeError("Can only choose 'muon' or 'electron'!!")
 
 def transpose(hist2d):
 	xbins = set()
@@ -55,34 +64,55 @@ def graph2hist(graph):
 		ret[i+1].value = y
 
 #set_trace()
-trig = TFile.Open('EfficienciesAndSF_RunBtoF_Nov17Nov2017.root')
-lepid = TFile.Open('Run2018ABCD_SF_ID.root')
-iso = TFile.Open('Run2018ABCD_SF_ISO.root') #tracking eff as iso given is a 2D plot
-#trk = root_open('ratios.root')
+trg = 0
+lepid = 0
+lepiso = 0
 
-#trg = transpose(trig.Ele32_eta2p1_WPTight_Gsf__EffData)
-#trg = transpose(trig.Get('IsoMu27_PtEtaBins/pt_abseta_ratio'))
-# 
-# htrk = graph2hist(trk.ratio_eta)
+info = Hist(3, 0, 3, type='I')
+if args.lepton == 'muon':
+    lid = root_open('Muons/Run2018ABCD_SF_ID.root')
+    lepid = fill_oflow(lid.NUM_TightID_DEN_genTracks_pt_abseta.Clone())
+
+    iso = root_open('Muons/Run2018ABCD_SF_ISO.root') #tracking eff as iso given is a 2D plot
+    lepiso = fill_oflow(iso.NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta.Clone())
+
+        ## weights are hard coded, need to check!
+    trig_p1 = root_open('Muons/EfficienciesAndSF_2018Data_BeforeMuonHLTUpdate.root')
+    trig_p2 = root_open('Muons/EfficienciesAndSF_2018Data_AfterMuonHLTUpdate.root')
+    print '\n       Weights used for muon trigger effs are hardcoded. Need to check!!!\n'
+    # Muon run splitting found here: https://twiki.cern.ch/twiki/bin/view/CMS/MuonReferenceEffs2018
+    p1_weight = 0.15105962910078496 # lumi ratio before 316361
+    p2_weight = 0.8489403708992149 # lumi ratio after and including 316361
+    trg = trig_p1.IsoMu24_PtEtaBins.pt_abseta_ratio*p1_weight + trig_p2.IsoMu24_PtEtaBins.pt_abseta_ratio*p2_weight
+
+    info[0].value = 1 #0 pt as Y, 1 pt as X
+    info[1].value = 1 #trig SF in |eta| (1) of full eta (0)
+    info[2].value = 1 #ID SF in |eta| (1) of full eta (0)
+    info[3].value = 1 #Iso SF in |eta| (1) of full eta (0)
+
+if args.lepton == 'electron':
+    lid = root_open('Electrons/2018_ElectronTight.root')
+    lepid = fill_oflow(lid.EGamma_SF2D.Clone())
+
+    trig = root_open('Electrons/electron_sf_Moriond17_TightCutID.root')
+    trg = trig.trg.Clone()
+
+    info[0].value = 0 #0 pt as Y, 1 pt as X
+    info[1].value = 0 #trig SF in |eta| (1) of full eta (0)
+    info[2].value = 0 #ID SF in |eta| (1) of full eta (0)
+    info[3].value = 0 #Iso SF in |eta| (1) of full eta (0)
+
+#set_trace()
+if trg == 0 or lepid == 0:
+    raise RuntimeError('Lepton trigger AND id info must be provided!')
 
 #read itself and dump
 code = TObjString(open('merge_sf.py').read())
 
-info = Hist(3, 0, 3, type='I')
-info[0].value = 1 #0 pt as Y, 1 pt as X
-info[1].value = 1 #trig SF in |eta| (1) of full eta (0)
-info[2].value = 1 #ID SF in |eta| (1) of full eta (0)
-info[3].value = 1 #Iso SF in |eta| (1) of full eta (0)
-#info[4].value = 1 #tracking SF in |eta| (1) of full eta (0)
-with File.open('output.root', 'w') as out:
-#with TFile.Open('output.root', 'w') as out:
-	set_trace()
-	#out.WriteTObject(fill_oflow(trig.Get('IsoMu27_PtEtaBins/pt_abseta_ratio')), 'trg')
-	#out.WriteTObject(fill_oflow(lepid.NUM_TightID_DEN_genTracks_pt_abseta.Clone()), 'id')
-	#out.WriteTObject(fill_oflow(iso.NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta.Clone()), 'iso')
-	out.WriteTObject(trig.Get('IsoMu27_PtEtaBins/pt_abseta_ratio').Clone(), 'trg')
-	out.WriteTObject(lepid.Get('NUM_TightID_DEN_genTracks_pt_abseta').Clone(), 'id')
-	out.WriteTObject(iso.Get('NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta').Clone(), 'iso')
-	# out.WriteTObject(htrk, 'trk')
+with root_open('output.root', 'w') as out:
+	#set_trace()
+	out.WriteTObject(trg, 'trg')
+	out.WriteTObject( lepid, 'id')
+	if args.lepton == 'muon': out.WriteTObject( lepiso, 'iso')
 	out.WriteTObject(info, 'info')
 	out.WriteTObject(code, 'code')
